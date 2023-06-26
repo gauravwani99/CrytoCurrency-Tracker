fc501a9f678f7e1c7baf586a25a208d73e25ae82
gauravwani99 <gauravwani405405@gmail.com>
2023-06-26 21:24:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] =useState(''); // empty string
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en')
+    .then(res => {
+      setCoins(res.data);
+      console.log(res.data);
+    }).catch(error => console.log(error))
+  }, []);
+
+  const handleChange = e =>{ // the handle change function 
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins =coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase()) // this helps us to match our searched coin , basically it becomes case insensitive
+    ) // this function filters all the coins
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className='coin-text'>Search a Crytocurrency</h1>
+        <form>
+          <input type='text' placeholder='Search' className='coin-input'  onChange={handleChange}/>
+        </form>
+
+      </div>
+      {filteredCoins.map(coin =>{
+        return <Coin key={coin.id} 
+        name={coin.name} 
+        image={coin.image} 
+        symbol={coin.symbol}
+        marketcap={coin.market_cap}
+        price={coin.current_price}
+        priceChange={coin.price_change_percentage_24h}
+        volume={coin.total_volume}
+        />;
+        
+      })}
+    </div>
+  );
+}
+
+export default App;
